1fb0a8167e43ec9b1033f51833e4c5d75121b122
pitardeveloper <37588350+pitardeveloper@users.noreply.github.com>
2018-03-20 19:56:03 +0300

    Update leveling_crediting.js

--- leveling_crediting.js
+++ leveling_crediting.js
@@ -1,3 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
 const sql = require("sqlite");
 const Discord = require("discord.js");
 const client = new Discord.Client();
