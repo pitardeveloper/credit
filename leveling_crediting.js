11c48a2080086febcf7fbc6b237ce60d1d740aee
pitardeveloper <37588350+pitardeveloper@users.noreply.github.com>
2018-03-20 19:45:32 +0300

    Create leveling_crediting.js

---
+++ leveling_crediting.js
@@ -0,0 +1,49 @@
+const sql = require("sqlite");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+sql.open("./score.sqlite");
+
+const prefix = "!";
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (message.channel.type !== "text") return;
+
+  sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+    if (!row) {
+      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
+    } else {
+      let curLevel = Math.floor(0.1 * Math.sqrt(row.points + 1));
+      if (curLevel > row.level) {
+        row.level = curLevel;
+        sql.run(`UPDATE scores SET points = ${row.points + 1}, level = ${row.level} WHERE userId = ${message.author.id}`);
+        message.reply(`You are leveled up to level **${curLevel}**`);
+      }
+      sql.run(`UPDATE scores SET points = ${row.points + 1} WHERE userId = ${message.author.id}`);
+    }
+  }).catch(() => {
+    console.error;
+    sql.run("CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER, level INTEGER)").then(() => {
+      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
+    });
+  });
+
+  if (!message.content.startsWith(prefix)) return;
+
+  if (message.content.startsWith(prefix + "level")) {
+    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+      if (!row) return message.reply("Your level is : 0");
+      message.reply(`Your level is : ${row.level}`);
+    });
+  } else
+
+  if (message.content.startsWith(prefix + "credit")) {
+    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+      if (!row) return message.reply(":atm: your credit card :credit_card: Money :moneybag: :dollar: is : 0 :dollar:");
+      message.reply(`your credit card :credit_card: Money :moneybag: :dollar: is : ${row.points} :dollar:`);
+    });
+  }
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
